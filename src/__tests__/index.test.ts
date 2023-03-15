4bdcbb6866a74b65c0b296246ea8b54e8a58fa0b
Sean Stocker <sean.stocker14@gmail.com>
2023-03-15 17:49:00 +0000

    Added utility for calculating the percentage from a given value and total.

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,6 +1,11 @@
-import { capitalise } from '../index';
+import { capitalise, calculatePercent } from '../index';
 
 test('capitalise', () => {
   expect(capitalise('sean')).toBe('Sean');
   expect(capitalise('sean stocker')).toBe('Sean stocker');
+});
+
+test('calculatePercent', () => {
+  expect(calculatePercent(3, 15)).toBe(20);
+  expect(calculatePercent(23, 46)).toBe(50);
 });
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,13 @@
 export function capitalise(str: string): string {
   return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
 }
+
+/**
+ * Calculates the percentage given a value and a total
+ * @param {number} value
+ * @param {number} total
+ * @return {number}
+ */
+export function calculatePercent(value: number, total: number): number {
+  return Math.round((value / total) * 100);
+}
