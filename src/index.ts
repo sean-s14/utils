383978e7f4326680ab0ca3e5c8d63890358731ba
Sean Stocker <sean.stocker14@gmail.com>
2023-04-03 22:13:45 +0100

    Created utility for randomly rearranging the items in an array in-place.

--- src/index.ts
+++ src/index.ts
@@ -211,3 +211,14 @@ export function rgbToHex(r: number, g: number, b: number): string | Error {
       .toUpperCase()
   );
 }
+
+/**
+ * Shuffles an array in-place
+ * @param {Array} array
+ */
+export function arrayShuffle<T>(array: T[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
